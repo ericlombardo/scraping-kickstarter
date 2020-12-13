dfe6412a515a759cd1da2bb1716083b181cd4fe8
Eric <eric.m.lombardo@gmail.com>
2020-12-13 18:22:14 -0500

    scraped kickstarter.html to create projects hash with title, image link, description, location, and percent_funded

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,32 @@
-# require libraries/modules here
+require 'nokogiri'  # require libraries/modules here
+require 'pry' 
+# HTML ELEMENTS NEEDED FOR ITERATION
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image linke: project.css("div.project-thumbail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css(".location-name").text
+# percent_funded: project.css(" ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html') # brings in HTML file from folder
+  
+  kickstarter = Nokogiri::HTML(html)    # NodeSet created using Nokogiri
+  
+  projects = {} # empty hash created to push all projects into
+  
+  kickstarter.css("li.project.grid_4").each do |project|  # Iterate through projects
+    title = project.css("h2.bbcard_name strong a").text   # set title to title of current project
+    
+    projects[title.to_sym] = {  # assign title to key and other data to value of the hash
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+  
+  projects  # returns the hash after all projects are pushed there  
 end
+
+create_project_hash # calls the method so that executes
